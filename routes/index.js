edf01934d3ece9950e85f0bdee90edcd485fd86e
Manthan Shah <manthanshah1711@gmail.com>
2019-02-16 08:52:26 -0800

    updated with resume link

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Manthan Shah' });
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('index', { title: 'About' });
+});
+
+router.get('/contact', (req, res, next) => {
+  res.render('index', { title: 'Contact' });
+});
+
+router.get('/products', (req, res, next) => {
+  res.render('index', { title: 'Projects' });
+});
+
+router.get('/services', (req, res, next) => {
+  res.render('index', { title: 'Services' });
+});
+
+
+module.exports = router;
